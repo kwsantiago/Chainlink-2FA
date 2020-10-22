4108ef5d2f405575c6f5b57ea469476da1adcaa4
kwsantiago <kwsantiago@usf.edu>
2020-10-22 15:38:39 -0400

    Add space in token result

--- 2fa-app/src/assets/utils/2fa.js
+++ 2fa-app/src/assets/utils/2fa.js
@@ -1,12 +1,13 @@
 import { vrfNumber } from "./smartContract"
 
 function formatVRF(vrfNum){
-    const result = vrfNum % 10000000;
+    const result = vrfNum % 100000000;
     if(result.length == 6)
         return result;
     const arrayOfDigits = Array.from(String(result), Number);
-    while(arrayOfDigits.length > 6)
+    while(arrayOfDigits.length > 7)
         arrayOfDigits.pop();
+    arrayOfDigits[Math.floor(arrayOfDigits.length / 2)] = ' ';
     return arrayOfDigits;
 }
 
